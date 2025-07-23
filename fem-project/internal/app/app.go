0e790c6e5e32d391ec437eb886d6f85c0d7079e5
Alejandro Ávila <alejandroavi95@hotmail.com>
2025-07-23 17:34:22 -0500

    06 - API CRUD Routes

---
+++ fem-project/internal/app/app.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"example.com/femproject/internal/api"
+	"example.com/femproject/internal/store"
+	"example.com/femproject/migrations"
+)
+
+type Application struct {
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
+}
+
+func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// our stores will go here
+
+	// our handlers will go here
+	workoutHandler := api.NewWorkoutHandler()
+
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
+	}
+
+	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Status is available\n")
+}
